507d0ccb6d3fb16566137623c9f916c8c6f8cb69
NeftaliLimon <nef.nl66@gmail.com>
2022-05-03 08:59:08 -0500

    Examen

---
+++ src/main/java/edu/uaslp/objetos/shoppingcart/Item.java
@@ -0,0 +1,50 @@
+package edu.uaslp.objetos.shoppingcart;
+
+import java.math.BigDecimal;
+
+public class Item {
+    String ItemCode;
+    String ProviderCode;
+    int Quantity;
+    BigDecimal UnitCost;
+
+    public Item(String itemCode, String providerCode, int quantity, BigDecimal unitCost ){
+        this.ItemCode = itemCode;
+        this.ProviderCode = providerCode;
+        this.Quantity = quantity;
+        this.UnitCost = unitCost;
+    }
+    public Item(){}
+
+    public void setCode(String itemCode){
+        this.ItemCode=itemCode;
+    }
+
+    public void setProviderCode(String providerCode){
+        this.ProviderCode=providerCode;
+    }
+
+    public void setQuantity(int quantity){
+        this.Quantity= quantity;
+    }
+
+    public void setUnitCost(BigDecimal unitCost){
+        this.UnitCost=unitCost;
+    }
+
+    public String getCode(){
+        return ItemCode;
+    }
+
+    public String getProviderCode(){
+        return ProviderCode;
+    }
+
+    public int getQuantity(){
+        return Quantity;
+    }
+
+    public BigDecimal getUnitCost(){
+        return UnitCost;
+    }
+}
--- src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
@@ -1,4 +1,41 @@
 package edu.uaslp.objetos.shoppingcart;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    List<Item> shoppingCartList = new LinkedList<>();
+
+    public boolean isEmpty(){
+        return shoppingCartList.isEmpty();
+    }
+
+    public BigDecimal getTotalCost(){
+        BigDecimal totalCost = BigDecimal.ZERO;
+        int size = shoppingCartList.size();
+
+        for(Item item: items){
+            totalCost = totalCost.add(item.getUnitCost().multiply(BigDecimal.valueOf(item.getQuantity())));
+        }
+
+        if(size == 0){
+            throw new EmptyShoppingCartException("cart is null");
+        }
+        return totalCost;
+    }
+
+    public void addItem(Item item){
+        shoppingCartList.add(item);
+    }
+
+    public int getItemsCount(){
+        int itemsCount = shoppingCartList.size();
+        return itemsCount;
+    }
+
+    public List<Item> getItems(){
+        return shoppingCartList;
+    }
 }
